41a47adb463b56162b36aeda411c7536b60cb39c
Juan Jose Fernandez <fernandez.juan.j@gmail.com>
2020-05-13 23:30:50 -0400

    Minimize HTML to be only the essential Nav bar and Demo dropdown menus.

--- src/App.js
+++ src/App.js
@@ -51,112 +51,53 @@ class App extends React.Component {
   return (
     
     <div className="App">
+    
+    <head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous"/>
+    </head>
 
-<head>
-    <meta charset="utf-8"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <meta name="description" content="A layout example that shows off a responsive product landing page."/>
-    <title>Landing Page &ndash; Layout Examples &ndash; Pure</title>
-    <link rel="stylesheet" href="/css/pure/pure-min.css"></link>
-    <link rel="stylesheet" href="/css/pure/grids-responsive-min.css"></link>
-    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css"></link>
-    <link rel="stylesheet" href="/layouts/marketing/styles.css"></link>
-</head>
-<body>
+    <body>
 
-<div class="header">
-    <div class="home-menu pure-menu pure-menu-horizontal pure-menu-fixed">
-        <a class="pure-menu-heading" href="">Your Site</a>
+      {/* Nav Bar */}
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <img src="https://cdn-az.allevents.in/banners/34f81faeb09b04cf6c85354a6d3baa44-rimg-w2087-h1398-gmir.jpg" class="logo"></img>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
 
-        <ul class="pure-menu-list">
-            <li class="pure-menu-item pure-menu-selected"><a href="#" class="pure-menu-link">Home</a></li>
-            <li class="pure-menu-item"><a href="#" class="pure-menu-link">Tour</a></li>
-            <li class="pure-menu-item"><a href="#" class="pure-menu-link">Sign Up</a></li>
-        </ul>
-    </div>
-</div>
-
-<div class="splash-container">
-    <div class="splash">
-        <h1 class="splash-head">Email Manager</h1>
-        <p class="splash-subhead">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-        </p>
-        <p>
-            <a href="http://purecss.io" class="pure-button pure-button-primary">Get Started</a>
-        </p>
-    </div>
-</div>
-
-<div class="content-wrapper">
-    <div class="content">
-        <div class="pure-g">
-            <div class="l-box-lrg pure-u-1 pure-u-md-2-5">
-                <form class="pure-form pure-form-stacked">
-                    <fieldset>
-
-                    <div class="l-box-lrg pure-u-1 pure-u-md-2-5">
-
-                      {/* Drop Down Menu Test for Classes in Airtable */}
-                        <form action="/">
-                          <label for="class">Choose a class that you want to email:</label>
-                          <select id="class" name="classes">
-                              {/* these options should be generated based on what we have in Airtable */}
-                              {  listOfSessions }
-                          </select>
-                      </form>
-
-                        {/* Drop Down Menu Test for Email Templates in Mailchimp */}
-                        <form action="/">
-                          <label for="template">Choose an email template:</label>
-                          <select id="template" name="emails">
-                              { listOfEmailTemplates}
-
-                          </select>
-                      </form>
-                      </div>
-                    </fieldset>
-                </form>
-            </div>
-
-            </div>
-          </div>
-
-    <div class="ribbon l-box-lrg pure-g">
-        <div class="l-box-lrg is-center pure-u-1 pure-u-md-1-2 pure-u-lg-2-5">
-            <img width="300" alt="File Icons" class="pure-img-responsive" src="/img/common/file-icons.png"/>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+            <a class="nav-link" href="#">Schedule Emails</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Update Database <span class="sr-only">(current)</span></a>
+            </li>
+           </ul>
         </div>
-        <div class="pure-u-1 pure-u-md-1-2 pure-u-lg-3-5">
+      </nav>
 
-            <h2 class="content-head content-head-ribbon">Laboris nisi ut aliquip.</h2>
+      <div class = "menu">
+        {/* Drop Down Menu Test for Classes in Airtable */}
+        <form action="/" class="menu box">
+            <label for="class">Choose a class that you want to email:</label>
+            <select id="class" name="classes">
+                {/* these options should be generated based on what we have in Airtable */}
+                {  listOfSessions }
+            </select>
+        </form>
+        {/* Drop Down Menu Test for Email Templates in Mailchimp */}
+        <form action="/" class="menu box">
+            <label for="template">Choose an email template:</label>
+            <select id="template" name="emails">
+                { listOfEmailTemplates}
+            </select>
+        </form>
 
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat. Duis aute irure dolor.
-            </p>
         </div>
+      </body>  
     </div>
 
-    <div class="content">
-        <h2 class="content-head is-center">Dolore magna aliqua. Uis aute irure.</h2>
-    </div>
-
-    <div class="footer l-box is-center">
-        footer
-    </div>
-
-</div>
-
-</body>
-
-    </div>
-    
-    
-  
-
-    
   );
   }
 }
