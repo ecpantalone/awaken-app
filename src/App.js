dfc5902177669fe059f18404bfee5f05b9bc8404
Juan Jose Fernandez <fernandez.juan.j@gmail.com>
2020-05-19 15:06:24 -0400

    Create Campaign via Form Submission

--- src/App.js
+++ src/App.js
@@ -8,43 +8,39 @@ class App extends React.Component {
     super(props)
 
     this.state = {
-      sessions:["session 1", "session 2"]    
+      sessions:["session 1", "session 2"],
+      // submitClicked: false
     }
 
-    this.sessionListGenerator = this.sessionListGenerator.bind(this);
+    // this.sessionListGenerator = this.sessionListGenerator.bind(this);
+    // this.submitClicked = this.submitClicked.bind(this);
     //this.state = {
     //
     //};
     //
   }
   
+
   componentDidMount()
   {
-    
     console.log("let's see if this pops up");
   }
 
-  sessionListGenerator(session){
-    return (<option value={session}>{session}</option>);
-  }
+
 
   render() {
-  let listOfSessions = [];
-
-  listOfSessions = this.state.sessions.map(this.sessionListGenerator);
-
+ 
 
   return (
     
     <div className="App">
     
-    <head>
+    {/* <head>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous"/>
-    </head>
+    </head> */}
 
-    <body>
-
-      {/* Nav Bar */}
+    {/* <body> */}
+{/* NAV BAR
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <img src="https://cdn-az.allevents.in/banners/34f81faeb09b04cf6c85354a6d3baa44-rimg-w2087-h1398-gmir.jpg" class="logo"></img>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -61,29 +57,11 @@ class App extends React.Component {
             </li>
            </ul>
         </div>
-      </nav>
+      </nav> */}
 
-      <div class = "menu">
-        {/* Drop Down Menu Test for Classes in Airtable */}
-        <form action="/" class="menu box">
-            <label for="class">Choose a class that you want to email:</label>
-            <select id="class" name="classes">
-                {/* these options should be generated based on what we have in Airtable */}
-                {  listOfSessions }
-            </select>
-        </form>
-        {/* Drop Down Menu Test for Email Templates in Mailchimp */}
-        <form action="/" class="menu box">
-            <label for="template">Choose an email template:</label>
-            <select id="template" name="emails"> {  <MailChimp mode = "templateList"/> }</select>
-            <br></br>
-            <label for="list">Choose an email list:</label>
-            <select id="list" name="lists">{  <MailChimp mode = "emailList" /> }</select>  
-        </form>
-        </div>
-      </body>  
+      <MailChimp/>
+      {/* </body>   */}
     </div>
-
   );
   }
 }
--- src/mailchimp.js
+++ src/mailchimp.js
@@ -13,18 +13,20 @@ class MailChimp extends React.Component {
     this.state = {
       templatesIsLoading: true,
       emailIsLoading: true,
+      campaignSent: false,
       templates: [],
-      lists: []
+      lists: [],
+      Submit: false
     }
 
     // bind things here
     this.getTemplates = this.getTemplates.bind(this);
     this.getEmailLists = this.getEmailLists.bind(this);
+    this.createCampaign = this.createCampaign.bind(this);
     this.buildTemplate = this.buildTemplate.bind(this);
     this.buildList = this.buildList.bind(this);
 
   }
-
 
 
   getTemplates()
@@ -82,6 +84,154 @@ class MailChimp extends React.Component {
 
   }
 
+  createCampaign()
+  {
+    if(!this.state.campaignSent){
+    console.log("I'm in the createCampaign function.");
+    let myHeaders = new Headers();
+    myHeaders.append("Cookie", "_mcid=1.1ebcc928d439d2d3d9b1ee09a070b504.fbf36732a11449a3cbe72c71f552661db989f91e30f901f9e5eb6eb113c3df30; _AVESTA_ENVIRONMENT=prod; ak_bmsc=70770938853E094C4C89D207F54998B8ACE80944FE5200004C97BC5E6973545B~pledBkBuDC75UQmGXG+Rvhktxr4MEwhQm5wEHWNHuPkVZ7mJUCrfl0tQeug2qPlOqEU8kJ89e1zd/mrUSXGTshWgZP9r8UYMvgzVNeUwxNUg0Re0Y/9Q6fOPjbm0PdbqUGWDy7APvxgXy16gf8GiQE3OnraLSrNSEHggOnelIJJpR8hzsaHl9A2hp9pzOhjo5Gh6/FqRTtIZEninbcbZbhFQOWbkQOH1eyjUz15C5EXuo=; bm_sv=7B1CEF76FA1F5E074266407ED0792CC4~ZCIJiXMoHGWdaOgTpvw6nB0hVzKmRFrZC8xDv4RPKSVN/c/dSAN27kp60ihLtV5fCdQ7jt2Y8LotFx4gNzAJE0ghnilEFpRycDZ82yoJ9oG74jRV6UUrInc5qHGibHh9CDnxm0AX4Kt8CJ9mtW62oLMgINaJW+3IaUVWBAbNO9c=");
+  
+    let requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      redirect: 'follow',
+      body:
+          {
+              //id: "",
+              //web_id: 1384388,
+              type: "regular",
+              create_time: "2020-05-14T14:15:19+00:00",
+              //archive_url: "http://eepurl.com/g3xd81",
+              //long_archive_url: "https://us18.campaign-archive.com/?u=19e4ec954436ef1a4b744c794&id=004b0eb929",
+              status: "save",
+              emails_sent: 0,
+              send_time: "",
+              content_type: "html",
+              //needs_block_refresh: false,
+              resendable: false,
+              recipients: {
+                  list_id: "5daa72e500",
+                  list_is_active: true,
+                  list_name: "Awaken Pittsburgh",
+                  segment_text: "",
+                  recipient_count: 16
+              },
+              settings: {
+                  subject_line: "You are now enrolled in Test Add!",
+                  preview_text: "Classes start on ",
+                  title: "Test Add - Added from Airtable",
+                  from_name: "Awaken Pittsburgh",
+                  reply_to: "awakenprjct@gmail.com",
+                  use_conversation: false,
+                  to_name: "*|FNAME|*",
+                  folder_id: "",
+                  authenticate: true,
+                  auto_footer: false,
+                  inline_css: false,
+                  auto_tweet: false,
+                  fb_comments: true,
+                  timewarp: false,
+                  template_id: 0,
+                  drag_and_drop: false
+              },
+              tracking: {
+                  opens: true,
+                  html_clicks: true,
+                  text_clicks: false,
+                  goal_tracking: false,
+                  ecomm360: false,
+                  google_analytics: "",
+                  clicktale: "N"
+              },
+              delivery_status: {
+                  enabled: false
+              },
+              _links: [
+                  {
+                      rel: "parent",
+                      href: "https://us18.api.mailchimp.com/3.0/campaigns",
+                      method: "GET",
+                      targetSchema: "https://us18.api.mailchimp.com/schema/3.0/Definitions/Campaigns/CollectionResponse.json",
+                      schema: "https://us18.api.mailchimp.com/schema/3.0/CollectionLinks/Campaigns.json"
+                  },
+                  {
+                      rel: "self",
+                      href: "https://us18.api.mailchimp.com/3.0/campaigns/004b0eb929",
+                      method: "GET",
+                      targetSchema: "https://us18.api.mailchimp.com/schema/3.0/Definitions/Campaigns/Response.json"
+                  },
+                  {
+                      rel: "delete",
+                      href: "https://us18.api.mailchimp.com/3.0/campaigns/004b0eb929",
+                      method: "DELETE"
+                  },
+                  {
+                      rel: "send",
+                      href: "https://us18.api.mailchimp.com/3.0/campaigns/004b0eb929/actions/send",
+                      method: "POST"
+                  },
+                  {
+                      rel: "cancel_send",
+                      href: "https://us18.api.mailchimp.com/3.0/campaigns/004b0eb929/actions/cancel-send",
+                      method: "POST"
+                  },
+                  {
+                      rel: "feedback",
+                      href: "https://us18.api.mailchimp.com/3.0/campaigns/004b0eb929/feedback",
+                      method: "GET",
+                      targetSchema: "https://us18.api.mailchimp.com/schema/3.0/Definitions/Campaigns/Feedback/CollectionResponse.json"
+                  },
+                  {
+                      rel: "content",
+                      href: "https://us18.api.mailchimp.com/3.0/campaigns/004b0eb929/content",
+                      method: "GET",
+                      targetSchema: "https://us18.api.mailchimp.com/schema/3.0/Definitions/Campaigns/Content/Response.json"
+                  },
+                  {
+                      rel: "send_checklist",
+                      href: "https://us18.api.mailchimp.com/3.0/campaigns/004b0eb929/send-checklist",
+                      method: "GET",
+                      targetSchema: "https://us18.api.mailchimp.com/schema/3.0/Definitions/Campaigns/Checklist/Response.json"
+                  },
+                  {
+                      rel: "pause",
+                      href: "https://us18.api.mailchimp.com/3.0/campaigns/004b0eb929/actions/pause",
+                      method: "POST"
+                  },
+                  {
+                      rel: "resume",
+                      href: "https://us18.api.mailchimp.com/3.0/campaigns/004b0eb929/actions/resume",
+                      method: "POST"
+                  },
+                  {
+                      rel: "replicate",
+                      href: "https://us18.api.mailchimp.com/3.0/campaigns/004b0eb929/actions/replicate",
+                      method: "POST"
+                  },
+                  {
+                      rel: "create_resend",
+                      href: "https://us18.api.mailchimp.com/3.0/campaigns/004b0eb929/actions/create-resend",
+                      method: "POST"
+                  }
+              ]
+          }
+        };
+    fetch(mailchimpURI + "campaigns?apikey=" + mailchimpAPIKey, requestOptions)
+      .then(response => {
+        return response.json();
+      })
+      .then(jsonData => {
+        console.log(jsonData);
+        this.setState({
+          campaigns: jsonData.campaigns,
+          campaignSent: true
+        });
+      })
+      .catch(error => console.log('error', error));
+
+  }
+}
+
   componentDidMount()
   {
      this.getTemplates();
@@ -100,23 +250,41 @@ class MailChimp extends React.Component {
 
   render (){
     let display = [];
-    
-    if(this.props.mode === "templateList"){
-      if (!this.state.templatesIsLoading){
-      display = this.state.templates.map(this.buildTemplate);
-      }
-    }
-    else if (this.props.mode === "emailList"){
-      if (!this.state.emailIsLoading){
-      display = this.state.lists.map(this.buildList);
-      }
-    }
+    let displayEmailList = [];
+    let displayTemplateList = [];
 
+    if (!this.state.templatesIsLoading){
+      displayTemplateList = this.state.templates.map(this.buildTemplate);
+    }
+      
+    if (!this.state.emailIsLoading){
+      displayEmailList = this.state.lists.map(this.buildList);
+    }
+    
 
     return(
-      <React.Fragment>
-      {display}
-      </React.Fragment>
+      // <React.Fragment>
+      // {display}
+      // </React.Fragment>
+      <div className = "menu">
+      {/* Drop Down Menu Test for Classes in Airtable */}
+      <form action="/" className="menu box">
+          <label htmlFor="class">Choose a class that you want to email:</label>
+          <select id="class" name="classes">
+              {/* these options should be generated based on what we have in Airtable */}
+          </select>
+      </form>
+      {/* Drop Down Menu Test for Email Templates in Mailchimp */}
+      <form action="/" className="menu box">
+          <label htmlFor="template">Choose an email template:</label>
+          <select id="template" name="emails"> { displayTemplateList}</select>
+          <br></br>
+          <label htmlFor="list">Choose an email list:</label>
+          <select id="list" name="lists">{ displayEmailList }</select>
+          {/*  we want to get that template and list and add it together to make a campaign (in POST/campaign)*/}
+          <input type="submit" key="submit" value={this.state.Submit} onClick={this.createCampaign()}></input>  
+      </form>
+      </div>
     );
     
   }
