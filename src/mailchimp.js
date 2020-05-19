2b7bafdda0129ebef7d98dc6037e6aafbd06b078
ecpantalone <ecpantalone@gmail.com>
2020-05-19 18:48:02 -0400

    remove cookies and update headers

--- src/mailchimp.js
+++ src/mailchimp.js
@@ -31,12 +31,11 @@ class MailChimp extends React.Component {
 
   getTemplates()
   {
-    let myHeaders = new Headers();
-    myHeaders.append("Cookie", "_mcid=1.1ebcc928d439d2d3d9b1ee09a070b504.fbf36732a11449a3cbe72c71f552661db989f91e30f901f9e5eb6eb113c3df30; _AVESTA_ENVIRONMENT=prod; ak_bmsc=70770938853E094C4C89D207F54998B8ACE80944FE5200004C97BC5E6973545B~pledBkBuDC75UQmGXG+Rvhktxr4MEwhQm5wEHWNHuPkVZ7mJUCrfl0tQeug2qPlOqEU8kJ89e1zd/mrUSXGTshWgZP9r8UYMvgzVNeUwxNUg0Re0Y/9Q6fOPjbm0PdbqUGWDy7APvxgXy16gf8GiQE3OnraLSrNSEHggOnelIJJpR8hzsaHl9A2hp9pzOhjo5Gh6/FqRTtIZEninbcbZbhFQOWbkQOH1eyjUz15C5EXuo=; bm_sv=7B1CEF76FA1F5E074266407ED0792CC4~ZCIJiXMoHGWdaOgTpvw6nB0hVzKmRFrZC8xDv4RPKSVN/c/dSAN27kp60ihLtV5fCdQ7jt2Y8LotFx4gNzAJE0ghnilEFpRycDZ82yoJ9oG74jRV6UUrInc5qHGibHh9CDnxm0AX4Kt8CJ9mtW62oLMgINaJW+3IaUVWBAbNO9c=");
-  
     let requestOptions = {
       method: 'GET',
-      headers: myHeaders,
+      headers: new Headers ({
+        "Authorization": `Basic ${new Buffer(`apikey:${mailchimpAPIKey}`).toString('base64')}`
+      }),
       redirect: 'follow'
     };
 
@@ -58,12 +57,11 @@ class MailChimp extends React.Component {
 
   getEmailLists()
   {
-    let myHeaders = new Headers();
-    myHeaders.append("Cookie", "_mcid=1.1ebcc928d439d2d3d9b1ee09a070b504.fbf36732a11449a3cbe72c71f552661db989f91e30f901f9e5eb6eb113c3df30; _AVESTA_ENVIRONMENT=prod; ak_bmsc=70770938853E094C4C89D207F54998B8ACE80944FE5200004C97BC5E6973545B~pledBkBuDC75UQmGXG+Rvhktxr4MEwhQm5wEHWNHuPkVZ7mJUCrfl0tQeug2qPlOqEU8kJ89e1zd/mrUSXGTshWgZP9r8UYMvgzVNeUwxNUg0Re0Y/9Q6fOPjbm0PdbqUGWDy7APvxgXy16gf8GiQE3OnraLSrNSEHggOnelIJJpR8hzsaHl9A2hp9pzOhjo5Gh6/FqRTtIZEninbcbZbhFQOWbkQOH1eyjUz15C5EXuo=; bm_sv=7B1CEF76FA1F5E074266407ED0792CC4~ZCIJiXMoHGWdaOgTpvw6nB0hVzKmRFrZC8xDv4RPKSVN/c/dSAN27kp60ihLtV5fCdQ7jt2Y8LotFx4gNzAJE0ghnilEFpRycDZ82yoJ9oG74jRV6UUrInc5qHGibHh9CDnxm0AX4Kt8CJ9mtW62oLMgINaJW+3IaUVWBAbNO9c=");
-  
     let requestOptions = {
       method: 'GET',
-      headers: myHeaders,
+      headers: new Headers ({
+        "Authorization": `Basic ${new Buffer(`apikey:${mailchimpAPIKey}`).toString('base64')}`
+      }),
       redirect: 'follow'
     };
 
@@ -88,8 +86,6 @@ class MailChimp extends React.Component {
   {
     if(!this.state.campaignSent){
     console.log("I'm in the createCampaign function.");
-    //let myHeaders = new Headers();
-    // myHeaders.append("Cookie", "_mcid=1.1ebcc928d439d2d3d9b1ee09a070b504.fbf36732a11449a3cbe72c71f552661db989f91e30f901f9e5eb6eb113c3df30; _AVESTA_ENVIRONMENT=prod; ak_bmsc=70770938853E094C4C89D207F54998B8ACE80944FE5200004C97BC5E6973545B~pledBkBuDC75UQmGXG+Rvhktxr4MEwhQm5wEHWNHuPkVZ7mJUCrfl0tQeug2qPlOqEU8kJ89e1zd/mrUSXGTshWgZP9r8UYMvgzVNeUwxNUg0Re0Y/9Q6fOPjbm0PdbqUGWDy7APvxgXy16gf8GiQE3OnraLSrNSEHggOnelIJJpR8hzsaHl9A2hp9pzOhjo5Gh6/FqRTtIZEninbcbZbhFQOWbkQOH1eyjUz15C5EXuo=; bm_sv=7B1CEF76FA1F5E074266407ED0792CC4~ZCIJiXMoHGWdaOgTpvw6nB0hVzKmRFrZC8xDv4RPKSVN/c/dSAN27kp60ihLtV5fCdQ7jt2Y8LotFx4gNzAJE0ghnilEFpRycDZ82yoJ9oG74jRV6UUrInc5qHGibHh9CDnxm0AX4Kt8CJ9mtW62oLMgINaJW+3IaUVWBAbNO9c=");
   
     let requestOptions = {
       method: 'POST',
