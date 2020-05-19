5a5b84232f3ecfd86ca7366dc0fc70b32875e0b4
katelynneuhaus <kneuhaus92@gmail.com>
2020-05-18 23:07:33 -0400

    DROP DOWN LIST IS WORKING!!!!!!

--- src/airtable.js
+++ src/airtable.js
@@ -18,73 +18,45 @@ class AirTable extends React.Component {
         super(props)
 
         this.state = {
-            sessions: ["default"]
+            sessions: []
         }
         this.getSessions = this.getSessions.bind(this);
-        this.addSession = this.addSession.bind(this);
         this.buildSession = this.buildSession.bind(this);
 
     }
     
     getSessions() {
-        let counter = 0;
+        var self = this;
         base('Session').select({
             view: "Grid view"
         }).eachPage(function page(records, fetchNextPage) {
 
             records.forEach(function (record) {
-                sessionsList.push(record.get('Session'));
-                console.log("inside of records loop in get sessions");
-                console.log("the airtable api is returning data type: " + typeof record.get('Session'))
-                console.log(sessionsList);
-                console.log("sessionsList is data type: " + typeof sessionsList)
-                console.log("the item in spot " + counter + " of sessionsList is " + sessionsList[counter])
-
-                counter += 1;
+                self.setState({ 
+                    sessions: self.state.sessions.concat([record.get('Session')])
+                  });
             });
             fetchNextPage();
 
         }, function done(err) {
-            console.log("inside of done in get sessions")
             if (err) { console.error(err); return; }
         });
-        console.log("outside of records loop in get sessions")
-        console.log(sessionsList);
-        console.log("the item in spot " + counter + " of sessionsList is " + sessionsList[counter])
-        // this.addSession(sessionsList[0]);
     }
 
     componentDidMount() {
-        console.log("inside of component did mount");
         this.getSessions();
-        console.log("after get sessions / bfore add session in component did mount");
-        this.addSession(sessionsList[0]);
-    }
-
-    addSession(s) {
-        console.log("inside of add session")
-        console.log("adding " + s + " to this.state.sessions")
-        this.setState({ 
-            sessions: this.state.sessions.concat([s])
-          });
-          return "";
     }
 
     buildSession(s) {
-        console.log("inside of build sessions");
         return (<option value={s}>{s}</option>);
     }
 
     render() {
-        console.log("inside of render");
-
         let displaySessions = [];
         displaySessions = this.state.sessions.map(this.buildSession);
 
         return (
             <React.Fragment>
-                {console.log(this.state.sessions)}
-                {console.log(displaySessions)}
                 {displaySessions}
             </React.Fragment>
         );
