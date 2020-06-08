459939635722ca7e8a7cfa7ba64c435762f6bea0
katelynneuhaus <kneuhaus92@gmail.com>
2020-06-08 11:30:05 -0400

    moved app.js content to dashboard.js

---
+++ src/apirequestform.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import './App.css';
+import './flatpickr.css';
+import AirTable from './airtable';
+import Formmenu from './formmenu';
+import MailChimp from './mailchimp';
+import UpdateMailchimp from './updatemailchimp';
